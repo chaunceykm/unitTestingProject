dfe0288cd5cd736d7ff8332cef62bd9b6c089375
Kristen Chauncey <kristen.michelle82@gmail.com>
2020-10-23 15:05:24 -0500

    add phase 3 solution shell

--- test_solution/phase-3-spec-solution.js
+++ test_solution/phase-3-spec-solution.js
@@ -2,4 +2,48 @@ const chai = require("chai");
 const expect = chai.expect;
 
 const { Word } = require('../problems/phase-3')
-
+describe('Word', function () {
+  describe('Word constructor function', function () {
+    it('should have a "word" property', function() {
+      //Arrange
+      
+      //Act
+      
+      //Assert
+    })
+     it('should set the "word" property when a new word is created', function() {
+      //Arrange
+      
+      //Act
+      
+      //Assert
+    })
+  })
+  describe('removeVowels function', function () {
+    it('should return a the word with all vowlels removed', function() {
+      //Arrange
+      
+      //Act
+      
+      //Assert
+    })
+  })
+  describe('removeConsonents function', function () {
+})
+  it('should return the word with the consonents removed', function() {
+    //Arrange
+    
+    //Act
+    
+    //Assert
+  })
+  describe('pigLatin function', function () {
+})
+  it('should return the word converted to pig latin', function() {
+    //Arrange
+    
+    //Act
+    
+    //Assert
+  })
+})
